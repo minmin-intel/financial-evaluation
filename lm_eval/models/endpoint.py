66e0d89e848096a756ce3287897bf8e61e77f867
minmin-intel <minmin.hou@intel.com>
2024-12-19 01:03:38 +0000

    adaption of pixiu finben

---
+++ lm_eval/models/endpoint.py
@@ -0,0 +1,284 @@
+import os
+import numpy as np
+import transformers
+from lm_eval.base import BaseLM
+from lm_eval import utils
+from tqdm import tqdm
+import time
+
+
+def get_result(response, ctxlen):
+    """Process results from OpenAI API response.
+
+    :param response: dict
+        OpenAI API Response
+    :param ctxlen: int
+        Length of context (so we can slice them away and only keep the predictions)
+    :return:
+        continuation_logprobs: np.array
+            Log probabilities of continuation tokens
+        is_greedy: bool
+            whether argmax matches given continuation exactly
+    """
+    is_greedy = True
+    logprobs = response["logprobs"]["token_logprobs"]
+    continuation_logprobs = sum(logprobs[ctxlen:])
+
+    for i in range(ctxlen, len(response["logprobs"]["tokens"])):
+        token = response["logprobs"]["tokens"][i]
+        top_tokens = response["logprobs"]["top_logprobs"][i]
+        top_token = max(top_tokens.keys(), key=lambda x: top_tokens[x])
+        if top_token != token:
+            is_greedy = False
+            break
+
+    return continuation_logprobs, is_greedy
+
+
+def oa_completion(**kwargs):
+    """Query OpenAI API for completion.
+
+    Retry with back-off until they respond
+    """
+    import openai
+
+    backoff_time = 3
+    while True:
+        try:
+            return openai.Completion.create(**kwargs)
+        except openai.error.OpenAIError:
+            import traceback
+
+            traceback.print_exc()
+            time.sleep(backoff_time)
+            backoff_time *= 1.5
+
+
+def chat_completion(message, **kwargs):
+    from openai import OpenAI
+
+    print(kwargs)
+
+    endpoint_url = kwargs["endpoint_url"]
+    print(f"** Endpoint URL: {endpoint_url}")
+
+    client = OpenAI(
+            base_url=endpoint_url,
+            api_key="EMPTY_KEY",
+        )
+    
+    completion = client.chat.completions.create(
+        model=kwargs["model_name"],
+        messages=[
+            {"role": "user", "content": message}
+        ],
+        temperature=kwargs["temperature"],
+        max_tokens=kwargs["max_tokens"],
+        )
+
+    res = completion.choices[0].message.content
+    print(f"** Response: {res}")
+    return res
+    
+
+
+class EndpointLM(BaseLM):
+    REQ_CHUNK_SIZE = 20
+
+    def __init__(self, model_name, endpoint_url, truncate=False):
+        """
+
+        :param engine: str
+            OpenAI API engine (e.g. davinci)
+        :param truncate: bool
+            Truncate input if too long (if False and input is too long, throw error)
+        """
+        super().__init__()
+
+        self.model_name = model_name
+        self.endpoint_url = endpoint_url
+        self.tokenizer = transformers.AutoTokenizer.from_pretrained(model_name)
+
+        self.vocab_size = self.tokenizer.vocab_size
+
+        # to make the annoying "Using pad_token, but it is not set yet." error go away
+        # self.tokenizer.pad_token = "<|endoftext|>"
+        # assert self.tokenizer.encode("hello\n\nhello") == [31373, 198, 198, 31373]
+        self.truncate = truncate
+        # self.end_of_text_token_id = self.tokenizer.convert_tokens_to_ids(
+        #     ["<|endoftext|>"]
+        # )[0]
+
+        # # Read from environment variable OPENAI_API_SECRET_KEY
+        # openai.api_key = os.environ["OPENAI_API_SECRET_KEY"]
+
+    @property
+    def eot_token_id(self):
+        return self.tokenizer.eos_token_id
+
+    @property
+    def max_length(self):
+        # Note: the OpenAI API supports up to 2049 tokens, with the first token being the first input token
+        return 2048
+
+    @property
+    def max_gen_toks(self):
+        return 256
+
+    @property
+    def batch_size(self):
+        # Isn't used because we override _loglikelihood_tokens
+        raise NotImplementedError()
+
+    @property
+    def device(self):
+        # Isn't used because we override _loglikelihood_tokens
+        raise NotImplementedError()
+
+    def tok_encode(self, string: str):
+        return self.tokenizer.encode(string, add_special_tokens=False)
+
+    def tok_decode(self, tokens):
+        return self.tokenizer.decode(tokens)
+
+    def _loglikelihood_tokens(self, requests, disable_tqdm=False):
+        res = []
+
+        def _collate(x):
+            # this doesn't efficiently handle last-token differences yet, but those are kinda annoying because
+            # it's not guaranteed that the 100 or so logprobs we get to see actually contain all the continuations
+            # we care about and so we need some kind of backup for when it isn't
+            toks = x[1] + x[2]
+            return -len(toks), tuple(toks)
+
+        re_ord = utils.Reorderer(requests, _collate)
+
+        for chunk in tqdm(
+            list(utils.chunks(re_ord.get_reordered(), self.REQ_CHUNK_SIZE)),
+            disable=disable_tqdm,
+        ):
+            inps = []
+            ctxlens = []
+            for cache_key, context_enc, continuation_enc in chunk:
+                # max_length+1 because the API takes up to 2049 tokens, including the first context token
+                inp = (context_enc + continuation_enc)[-(self.max_length + 1) :]
+                # TODO: the logic is much simpler if we just look at the length of continuation tokens
+                ctxlen = len(context_enc) - max(
+                    0, len(context_enc) + len(continuation_enc) - (self.max_length + 1)
+                )
+
+                inps.append(inp)
+                ctxlens.append(ctxlen)
+
+            response = chat_completion(
+                message=inps,
+                model_name=self.model_name,
+                endpoint_url=self.endpoint_url,
+                max_tokens=1024,
+                temperature=0.0,
+            )
+
+            for resp, ctxlen, (cache_key, context_enc, continuation_enc) in zip(
+                response.choices, ctxlens, chunk
+            ):
+                answer = get_result(resp, ctxlen)
+
+                res.append(answer)
+
+                # partial caching
+                if cache_key is not None:
+                    self.cache_hook.add_partial("loglikelihood", cache_key, answer)
+
+        return re_ord.get_original(res)
+
+    def generate(self, requests):
+        print("===== Endpoint generate=====")
+        print("*** Requests: ", requests)
+        res = []
+        for req in requests:
+            prompt = req[0]
+            print(f"** Prompt: {prompt}")
+            response = chat_completion(
+                message=prompt,
+                model_name=self.model_name,
+                endpoint_url=self.endpoint_url,
+                max_tokens=1024,
+                temperature=0.0,
+            )
+            res.append(response)
+        return res
+
+    def greedy_until(self, requests):
+        if not requests:
+            return []
+        res = []
+        print("===== Endpoint greedy_until=====")
+        print("*** Requests: ", requests)
+        def _collate(x):
+            toks = self.tok_encode(x[0])
+            return len(toks), x[0]
+
+        re_ord = utils.Reorderer(requests, _collate)
+
+        def sameuntil_chunks(xs, size):
+            ret = []
+            lastuntil = xs[0][1]
+            for x in xs:
+                if len(ret) >= size or x[1] != lastuntil:
+                    yield ret, lastuntil
+                    ret = []
+                    lastuntil = x[1]
+                ret.append(x)
+
+            if ret:
+                yield ret, lastuntil
+
+        # todo: more intelligent batching for heterogeneous `until`
+        for chunk, until in tqdm(
+            list(sameuntil_chunks(re_ord.get_reordered(), self.REQ_CHUNK_SIZE))
+        ):
+            inps = []
+            for context, _ in chunk:
+                context_enc = self.tok_encode(context)
+                inp = context_enc[-(self.max_length - self.max_gen_toks) :]
+                inps.append(inp)
+
+            # response = oa_completion(
+            #     engine=self.engine,
+            #     prompt=inps,
+            #     max_tokens=self.max_gen_toks,
+            #     temperature=0.0,
+            #     logprobs=10,
+            #     stop=until,
+            # )
+
+            print(f"** inps: {inps}")
+
+            response = chat_completion(
+                message=inps,
+                model_name=self.model_name,
+                endpoint_url=self.endpoint_url,
+                max_tokens=1024,
+                temperature=0.0,
+            )
+
+            for resp, (context, until_) in zip(response.choices, chunk):
+                s = resp["text"]
+
+                for term in until_:
+                    s = s.split(term)[0]
+
+                # partial caching
+                self.cache_hook.add_partial("greedy_until", (context, until_), s)
+
+                res.append(s)
+
+        return re_ord.get_original(res)
+
+    def _model_call(self, inps):
+        # Isn't used because we override _loglikelihood_tokens
+        raise NotImplementedError()
+
+    def _model_generate(self, context, max_length, eos_token_id):
+        # Isn't used because we override greedy_until
+        raise NotImplementedError()
